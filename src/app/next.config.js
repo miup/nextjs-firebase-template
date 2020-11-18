166404d2121f694737e1b1ecec6c19794ddf2ade
miup <contact@miup.blue>
2020-11-18 14:31:03 +0900

    update config files

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,3 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
 module.exports = {
+  webpack(config) {
+    config.resolve.alias['~'] = path.resolve(__dirname)
+    return config
+  },
+  // css
+  cssModules: true,
   distDir: '../../dist/functions/next',
 }
